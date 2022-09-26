78ae39f5eff039af0c98eaab3058647802a19ba9
Ronit <ronitsavadimathrs@gmail.com>
2022-09-26 22:01:19 +0530

    EasyEd

--- app/src/main/java/com/example/easyed/LoginActivity.java
+++ app/src/main/java/com/example/easyed/LoginActivity.java
@@ -1,20 +1,41 @@
 package com.example.easyed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     Button button;
     TextView textView;
+    EditText iEmail, iPassword;
+    ProgressBar iProgressBar;
+    FirebaseAuth iFirebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        iEmail= findViewById(R.id.Lgmail);
+        iPassword= findViewById(R.id.Lpassword);
+        iFirebaseAuth= FirebaseAuth.getInstance();
+        iProgressBar= findViewById(R.id.progressBar);
+
 
         button= findViewById(R.id.signInButton);
         textView= findViewById(R.id.textView3);
@@ -27,12 +48,33 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentTwo= new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intentTwo);
-            }
-        });
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+                String Email=iEmail.getText().toString().trim();
+                String Password= iPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(Email) || TextUtils.isEmpty(Password)){
+                    iEmail.setError("Cannot be empty");
+                    iPassword.setError("Cannot be empty");
+                    return;
+                }
+
+                iProgressBar.setVisibility(View.GONE);
+
+                iFirebaseAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), SelectActivity.class));
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Error: "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+           }
+       });
     }
 }
--- app/src/main/java/com/example/easyed/RegisterActivity.java
+++ app/src/main/java/com/example/easyed/RegisterActivity.java
@@ -1,16 +1,32 @@
 package com.example.easyed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     Button button;
     TextView textView;
+    EditText rEmail, rPassword, rName;
+    ProgressBar progressBar;
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +34,16 @@ public class RegisterActivity extends AppCompatActivity {
 
         button= findViewById(R.id.signInButton);
         textView= findViewById(R.id.textView3);
+        rEmail= findViewById(R.id.Lgmail);
+        rName= findViewById(R.id.Lname);
+        rPassword= findViewById(R.id.Lpassword);
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser()!=null){
+            Toast.makeText(this, "Already Registered", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        }
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +56,37 @@ public class RegisterActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentTwo= new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intentTwo);
+                String Email= rEmail.getText().toString().trim();
+                String Password= rPassword.getText().toString().trim();
+                String Name= rName.getText().toString().trim();
+
+                if (TextUtils.isEmpty(Email) || TextUtils.isEmpty(Password) || TextUtils.isEmpty(Name)){
+                    rEmail.setError("Cannot be empty");
+                    rPassword.setError("Cannot be empty");
+                    rName.setError("Cannot be empty");
+                    return;
+                }
+
+
+
+                firebaseAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), SelectActivity.class));
+
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this, "Error: "+ Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+                    }
+                    progressBar.setVisibility(View.GONE);
+
+                    }
+                });
             }
         });
     }
